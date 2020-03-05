6a4edcef9e4d5498ecf665ae4df198b307dfefda
Radhika <radhi.virgo.16@gmail.com>
2020-03-05 14:20:54 -0600

    Contact Us

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -17,24 +17,7 @@ class NavBar extends Component{
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            {/*<Nav.Link href="#home">Home</Nav.Link>*/}
-                            {/*/!*<Nav.Link href="#link">Link</Nav.Link>*!/*/}
-                            {/*<NavDropdown title="Filters" id="basic-nav-dropdown" style={{fontSize:"2rem"}}>*/}
-                                {/*<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
-                                {/*<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>*/}
-                                {/*<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
-                                {/*<NavDropdown.Divider />*/}
-                                {/*<NavDropdown.Item href="#action/3.4">Price</NavDropdown.Item>*/}
-                                {/*<NavDropdown.Item href="#action/3.4"><input type="checkbox"/>&nbsp;$</NavDropdown.Item>*/}
-                                {/*<NavDropdown.Item href="#action/3.4"><input type="checkbox"/>&nbsp;$$</NavDropdown.Item>*/}
-                                {/*<NavDropdown.Item href="#action/3.4"><input type="checkbox"/>&nbsp;$$$</NavDropdown.Item>*/}
-                                {/*<NavDropdown.Item href="#action/3.4"><input type="checkbox"/>&nbsp;$$$$</NavDropdown.Item>*/}
-                            {/*</NavDropdown>*/}
                         </Nav>
-                        {/*<Form inline>*/}
-                            {/*<FormControl type="text" placeholder="Search" className="mr-sm-2" />*/}
-                            {/*<Button variant="outline-success">Search</Button>*/}
-                        {/*</Form>*/}
                     </Navbar.Collapse>
                 </Navbar>
             </div>
